499abce776e19d407601b47b868b37fea18fd238
Jeovanny <jeov94@gmail.com>
2017-11-29 14:09:44 -0600

    collapse-menu working

--- functions.php
+++ functions.php
@@ -11,3 +11,14 @@ function register_my_menus() {
 }
 add_action( 'init', 'register_my_menus' );
 ?>
+
+<?php
+function wpb_adding_scripts() {
+
+wp_register_script('collapse-menu', get_template_directory_uri().'/menu.js', array('jquery'), '1', true);
+
+wp_enqueue_script('collapse-menu');
+}
+
+add_action( 'wp_enqueue_scripts', 'wpb_adding_scripts' );
+?>
