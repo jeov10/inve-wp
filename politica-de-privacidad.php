8a1e9baf2bb329bd7b0f70f0d63dadfc85b07c7b
Jeovanny <jeov94@gmail.com>
2017-11-30 16:41:14 -0600

    add to wp politicas page

---
+++ politica-de-privacidad.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Template Name: Política de privacidad
+*/
+?>
+<?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+get_header(); ?>
+
+  <section>
+    <div class="container">
+      <div class="box__description u-border-bottom">
+        <p class="u-color-primary text-aling-center subscribe-form h1 --u-margin-bottom" >Aviso de confidencialidad</p>
+      </div>
+      <div class="box__description">
+        <p class="u-color-primary text-aling-justify">Este mensaje y cualquier archivo anexo incluido en el mismo (en lo sucesivo la “Información”) son enviados únicamente para el destinatario previsto y pueden contener información confidencial y/o privilegiada.
+          Si usted no es el destinatario previsto, por este medio se le notifica que cualquier copia, uso, o distribución de la Información se encuentra estrictamente prohibida y sujeta a las sanciones establecidas en las leyes correspondientes por lo que deberá notificarnos por E-mail y suprimir inmediatamente este mensaje de su sistema.
+          Asimismo, toda la Información, solo tiene propósitos informativos y no reflejan la voluntad de <span class="u-fw-bold">Desarrollo colectivo inmobiliario, S. A. P.I. DE C.V.,</span> Sociedad Financiera de Objeto Múltiple, Entidad No Regulada, en lo sucesivo <span class="u-fw-bold">Desarrollo colectivo inmobiliario, S. A. P.I. DE C.V.)</span>, hasta en tanto no sea suscrita de forma autógrafa por un representante legal con facultades suficientes para obligar a <span class="u-fw-bold">Desarrollo colectivo inmobiliario, S. A. P.I. DE C.V..</span> Aunque <span class="u-fw-bold">Desarrollo colectivo inmobiliario, S. A. P.I. DE C.V.</span> se haya empeñado en que la Información se envié libre de cualquier virus o cualquier otro defecto que pueda afectar su sistema, es responsabilidad del destinatario asegurarse de dicha circunstancia antes de abrirlos, por lo que <span class="u-fw-bold">Desarrollo colectivo inmobiliario, S. A. P.I. DE C.V.</span> no asume ninguna responsabilidad por ninguna perdida, menoscabo y/o perjuicio ocasionado al destinatario o a cualquier tercero ni por cualquier alteración o cambio que este mensaje llegase a sufrir durante su transferencia.</p>
+        </div>
+        <div class="box__notice --u-margin-top">
+          <div class="box__description u-border-bottom">
+            <p class="u-color-primary text-aling-center subscribe-form h1 --u-margin-bottom" >Confidentility Notice</p>
+          </div>
+          <div class="box__description">
+            <p class="u-color-primary text-aling-justify">This message and any attachments included in such message (hereinafter the “Information”) are sent to the intended recipient and may contain privileged and/or confidential information.
+              If you are not the intended recipient you are hereby notified that any use, copy, or distribution of the Information and any attachments is strictly prohibited and subject to the penalties set forth in the applicable laws and regulations, therefore, you shall advise the sender by Email and delete immediately this message from your system.
+              All the Information are for informational purposes only and under no circunstances should be considered as the will of <span class="u-fw-bold">Desarrollo colectivo inmobiliario S.A.P.I. de C.V.</span> Sociedad Anónima Promotora de Inversión de Capital Variable, (hereinafter <span class="u-fw-bold">Desarrollo colectivo inmobiliario, S. A. P.I. DE C.V.</span>), unless otherwise executed by the original written signature of a duly authorized representative properly entitled to bind <span class="u-fw-bold">Desarrollo colectivo inmobiliario, S. A. P.I. DE C.V.</span>.
+              Although <span class="u-fw-bold">Desarrollo colectivo inmobiliario, S. A. P.I. DE C.V.</span> has endeavor for the Information to be free of any virus or other defect that might affect any computer system, it is the responsibility of the recipient to ensure of such circunstance before open it and no responsibility is accepted by <span class="u-fw-bold">Desarrollo colectivo inmobiliario, S. A. P.I. DE C.V</span>. for any loss or damage caused to the recipient or any third party arising in any way from its use or for any alteration or change that this message may suffer during its transfer.­</p>
+            </div>
+          </div>
+        </div>
+  </section>
+<?php get_footer(); ?>
