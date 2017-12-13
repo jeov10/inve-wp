9f5edce5c6c6ed4c2ff06555dc5eff804942d39c
jeov10 <32721103+jeov10@users.noreply.github.com>
2017-12-13 17:38:58 -0600

    latest changes inside wordpress

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php get_header(); ?>
+
+
   <section id="main">
-    <?php
-      global $post;
-      echo apply_filters('the_content', $post->post_content);
-    ?>
-  </section>
-<?php get_footer(); ?>
+
+
+
+  </section> <!-- Fin de main -->
+
+
+
+  <?php get_footer(); ?>
